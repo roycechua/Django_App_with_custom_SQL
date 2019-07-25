93e8f74602611cf8e2cb470a2d738cede4aa2000
Royce <royce236@gmail.com>
2019-07-25 16:10:52 +0800

    added message to line 4

--- program1.py
+++ program1.py
@@ -1,3 +1,4 @@
 import random
 value = random.randint(1,6) # Changed Range 0,6 to 1,6 , Changed Function from randInt to randint. Prolly Works.
 print(f"The value is {value}")
+print("End of the program..")
