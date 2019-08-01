8e34a1c948d6b4aeab84bd7fefed088b56d43e4f
Royce <royce236@gmail.com>
2019-08-01 14:49:12 +0800

    Delete program1.py

--- program1.py
+++
@@ -1,4 +0,0 @@
-import random
-value = random.randint(1,6) # Changed Range 0,6 to 1,6 , Changed Function from randInt to randint. Prolly Works.
-print(f"The value is {value}")
-print("End of the program..")
